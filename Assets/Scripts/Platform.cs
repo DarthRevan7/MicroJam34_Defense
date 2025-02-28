96728c33014aa83de4e1697403457af35606172d
DarthRevan7 <83843764+DarthRevan7@users.noreply.github.com>
2025-02-28 17:29:55 +0100

    modificare bene input manager
    Fare in modo che quando ho già colpito una volta una piattaforma, resetto la UI di quella e poi metto la nuova.

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 using UnityEngine.EventSystems;
+using Unity.VisualScripting;
 
 public class Platform : MonoBehaviour
 {
@@ -14,7 +15,9 @@ public class Platform : MonoBehaviour
     [SerializeField] private Vector2 screenPosition;
     [SerializeField] private Vector2 offset = new Vector2(0,15);
     [SerializeField] private InputActionReference mouseClick, mousePosition;
-
+    [SerializeField] private static float screenBounds = 15f;
+    [SerializeField] private bool activeUI = false;
+ 
     void Awake()
     {
         platformCenter = transform.Find("Platform_Center");
@@ -37,7 +40,28 @@ public class Platform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(activeUI)
+        {
+            UpdateUIPosition();
+        }
+    }
+
+    public void UpdateUIPosition()
+    {
+        //Ricavo la posizione sullo schermo del punto in cui deve comparire la UI
+        screenPosition = Camera.main.WorldToScreenPoint(centerPosition);
+        screenPosition += offset;
+        //Se la UI è fuori dai bounds
+        //Forse devo modificare in modo da capire se il rect transform è fuori dai bounds
+        //oppure uno dei bordi sta andando fuori dal bound.
+        if(screenPosition.x > Screen.width - screenBounds || screenPosition.x < screenBounds || screenPosition.y > Screen.height - screenBounds || screenPosition.y < screenBounds )
+        {
+            buildTurretPanel.SetActive(false);
+            return;
+        }
+
+        //Set UI Position
+        buildTurretPanel.GetComponent<RectTransform>().position = screenPosition;
     }
 
     public void SetUI()
@@ -53,11 +77,15 @@ public class Platform : MonoBehaviour
         buildTurretPanel.GetComponent<RectTransform>().position = screenPosition;
         //Activate UI gameobject
         buildTurretPanel.SetActive(true);
+        activeUI = true;
     }
 
     public void ResetUI()
     {
-        buildTurretPanel.SetActive(false);
+        activeUI = false;
+        buildTurretPanel.SetActive(activeUI);
     }
+
+    
     
 }
